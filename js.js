98459c5748a3c49d348f6230b5bf6dc35a4a785b
student <you@example.com>
2018-08-15 10:00:02 -0500

    check in

---
+++ js.js
@@ -0,0 +1,22 @@
+document.getElementById("demo").innerHTML =
+    "Screen Width: " + screen.width;
+
+document.getElementById("demo").innerHTML =
+    "Screen Height: " + screen.height;
+
+document.getElementById("demo").innerHTML =
+    "Available Screen Width: " + screen.availWidth;
+
+document.getElementById("demo").innerHTML =
+    "Available Screen Height: " + screen.availHeight;
+
+document.getElementById("demo").innerHTML =
+    "Screen Color Depth: " + screen.colorDepth;
+
+document.getElementById("demo").innerHTML =
+    "Screen Pixel Depth: " + screen.pixelDepth;
+
+window.location.hostname;
+window.location.href;
+window.location.pathname;
+window.location.protocol;
